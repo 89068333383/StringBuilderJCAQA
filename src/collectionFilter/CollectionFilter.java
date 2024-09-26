b3bdaaa718008ead659091972525c627937be88e
89068333383 <89068333383@mail.ru>
2024-09-26 10:30:31 +0500

    Напишите метод filter, который принимает на вход массив любого типа, вторым аргументом метод должен принимать класс, реализующий интерфейс Filter, в котором один метод - Object apply(Object o). Метод должен быть реализован так чтобы возвращать новый массив, к каждому элементу которого была применена функция apply

--- src/collectionFilter/CollectionFilter.java
+++ src/collectionFilter/CollectionFilter.java
@@ -3,7 +3,7 @@ package collectionFilter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class collectionFilter {
+public class CollectionFilter {
     List<Object> filter(List<Object> list, FilterImpl filter){
         List<Object> filteredList = new ArrayList<Object>();
         for(Object o : list){
